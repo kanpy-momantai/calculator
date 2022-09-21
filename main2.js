810b74c2bf4a614430d6d70332b1919be322476d
kanpy-momantai <kanpy.mocky@gmail.com>
2022-09-22 04:03:56 +0900

    小数点を複数押せる不具合を修正

--- main2.js
+++ main2.js
@@ -1,11 +1,12 @@
 var input = "";
+var point_input = false;
 $(document).ready(function(){
    $(".normal-number").click(function(){
        if(($(".result").text() != "0") && ($(".result").text() != ".")){
         input = $(".result").text();
         input = input + $(this).text();
        }
-    $(".result").text(input);
+   $(".result").text(input);
    });
    
    $(".four-arithmetic-operations").click(function(){
@@ -14,41 +15,45 @@ $(document).ready(function(){
         input = $(".result").text();
         input = input + $(this).text();
         $(".result").text(input);
+        point_input = false;
        }
        else if($(this).text() == "-" && result.length == 0){
-           input = $(".result").text();
-           input = input + $(this).text();
+        input = $(".result").text();
+        input = input + $(this).text();
         $(".result").text(input);
+        point_input = false;
        }
    });
    
    $(".zero").click(function(){
        if($(".result").text() != "0"){
-    　　 input = $(".result").text();
+    　 　 input = $(".result").text();
       　 input = input + $(this).text();
-       $(".result").text(input);
+        $(".result").text(input);
        }
    });
    
    $(".double-zero").click(function(){
        if($(".result").text().length != 0 && $(".result").text() != "0"){
-           input = $(".result").text();
-           input = input + $(this).text();
+        input = $(".result").text();
+        input = input + $(this).text();
        $(".result").text(input);
        }
-   })
+   });
    
    $(".point").click(function(){
-        var result = $(".result").text();
-       if(result.length != 0 && result[result.length-1] != "+" && result[result.length-1] != "-" && result[result.length-1] != "*" && result[result.length-1] != "/" && result[result.length-1] != "."){
-        　 input = $(".result").text();
-           input = input + $(this).text();
-           $(".result").text(input);
+       var result = $(".result").text();
+       if(result.length != 0 && result[result.length-1] != "+" && result[result.length-1] != "-" && result[result.length-1] != "*" && result[result.length-1] != "/" && result[result.length-1] != "." && point_input == false){
+         input = $(".result").text();
+         input = input + $(this).text();
+         point_input = true;
+         $(".result").text(input);
        }
-   })
+    });
    
    $(".all-clear").click(function(){
        $(".result").text("");
+       point_input = false;
    });
    
    $(".equall").click(function() {
